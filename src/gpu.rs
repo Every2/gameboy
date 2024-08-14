28f7316bffaac11f1d08db377af56e284755de87
Every <christiansantoslima21@gmail.com>
2024-08-13 21:11:42 -0300

    feat: add oam to gpu

--- src/gpu.rs
+++ src/gpu.rs
@@ -6,23 +6,69 @@ enum TilePixelValue {
     Three
 }
 
-type Tile = [[TilePixelValue;8]; 8];
+type Tile = [[TilePixelValue; 8]; 8];
 
 fn empty_tile() -> Tile {
     [[TilePixelValue::Zero; 8]; 8]
+}
+
+#[derive(Clone, Copy)]
+enum Pallete {
+    Zero, 
+    One
+}
+
+#[derive(Clone, Copy)]
+enum TileMap {
+    X9800,
+    X9C00,
+}
+
+#[derive(Clone, Copy)]
+struct Sprite {
+    x: u8,
+    y: u8,
+    tile: u8,
+    x_flip: bool,
+    y_flip: bool,
+    priority: bool,
+    pallete: Pallete,
+}
+
+impl Sprite {
+    pub fn new() -> Sprite {
+        Sprite {
+            x: 0,
+            y: 0,
+            tile: 0,
+            x_flip: false,
+            y_flip: false,
+            priority: false,
+            pallete: Pallete::Zero
+        }
+    }
 }
 
 struct GPU {
     //VRAM: usize = 0x8000 - 0x9FFF + 1
     vram: [u8; 0x2000],
     tile_set: [Tile; 384],
+    //Gameboy ppu can display 40 objects (sprites)
+    sprite: [Sprite; 40],
+    oam: [u8; 0xA0],
+    window_tile_map: TileMap,
+    background_tile_map: TileMap,
 }
 
 impl GPU {
     fn new() -> GPU {
         GPU {
             vram: [0; 0x2000],
-            tile_set: [empty_tile(); 384]
+            tile_set: [empty_tile(); 384],
+            sprite: [Sprite::new(); 40],
+            oam: [0; 0xA0],
+            window_tile_map: TileMap::X9800,
+            background_tile_map: TileMap::X9800,
         }
     }
 
@@ -58,4 +104,32 @@ impl GPU {
             self.tile_set[tile_index][row_index][pixel_index] = value;
         }
     }
+
+    fn write_aom(&mut self, index: usize, value: u8) {
+        self.oam[index] = value;
+        let object_index = index / 4;
+        //Gameboy ppu can display 40 objects (sprites)
+        if object_index > 40 {return};
+
+        let byte = index % 4;
+
+        let data = self.sprite.get_mut(object_index).unwrap();
+        match byte {
+            0 => data.y = value,
+            1 => data.x = value,
+            2 => data.tile = value,
+            _ => {
+                data.pallete = if (value & 0x10) != 0 {
+                    Pallete::One
+                } else {
+                    Pallete::Zero
+                };
+                data.x_flip = (value & 0x20) != 0;
+                data.y_flip = (value & 0x40) != 0;
+                data.priority = (value & 0x80) == 0;
+            }
+        }
+    }
+
+    
 }
