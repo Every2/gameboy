1c699c44ea5f798add5aaca90a59d9950c1c67bc
Every <christiansantoslima21@gmail.com>
2024-08-16 16:11:14 -0300

    feat: implement step functions and some interruptss
    q

--- src/gpu.rs
+++ src/gpu.rs
@@ -18,6 +18,48 @@ enum Pallete {
     One
 }
 
+#[derive(Eq, PartialEq)]
+enum Interrupts {
+    None, 
+    VBlank,
+    LCDStat,
+    Both,
+}
+
+#[derive(Clone, Copy, PartialEq)]
+enum Mode {
+    Horizontal,
+    Vertical,
+    OAM,
+    VRAM
+}
+
+impl std::convert::From<Mode> for u8 {
+    fn from(value: Mode) -> Self {
+        match value {
+            Mode::Horizontal => 0,
+            Mode::Vertical => 1,
+            Mode::OAM => 2,
+            Mode::VRAM => 3,
+        }
+    }
+}
+
+impl Interrupts {
+    fn add(&mut self, other: Interrupts) {
+        match self {
+            Interrupts::None => *self = other,
+            Interrupts::VBlank if other == Interrupts::LCDStat => {
+                *self = Interrupts::Both
+            }
+            Interrupts::LCDStat if other == Interrupts::VBlank => {
+                *self = Interrupts::Both
+            }
+            _ => {}
+        };
+    }
+}
+
 #[derive(Clone, Copy)]
 enum TileMap {
     X9800,
@@ -49,6 +91,7 @@ impl Sprite {
     }
 }
 
+
 struct GPU {
     //VRAM: usize = 0x8000 - 0x9FFF + 1
     vram: [u8; 0x2000],
@@ -58,6 +101,15 @@ struct GPU {
     oam: [u8; 0xA0],
     window_tile_map: TileMap,
     background_tile_map: TileMap,
+    cycles: u16,
+    lcd_display_enabled: bool,
+    draw_mode: Mode,
+    line: u8,
+    vblank_interrupt_enabled: bool,
+    oam_interrupt_enabled: bool,
+    line_check: u8,
+    line_equals_line_check_interrupt_enabled: bool,
+    hblank_interrupt_enabled: bool,
 }
 
 impl GPU {
@@ -69,6 +121,15 @@ impl GPU {
             oam: [0; 0xA0],
             window_tile_map: TileMap::X9800,
             background_tile_map: TileMap::X9800,
+            cycles: 0,
+            lcd_display_enabled: false,
+            draw_mode: Mode::Horizontal,
+            line: 0,
+            vblank_interrupt_enabled: false,
+            oam_interrupt_enabled: false,
+            line_check: 0,
+            line_equals_line_check_interrupt_enabled: false,
+            hblank_interrupt_enabled: false,
         }
     }
 
@@ -105,7 +166,7 @@ impl GPU {
         }
     }
 
-    fn write_aom(&mut self, index: usize, value: u8) {
+    fn write_oam(&mut self, index: usize, value: u8) {
         self.oam[index] = value;
         let object_index = index / 4;
         //Gameboy ppu can display 40 objects (sprites)
@@ -131,5 +192,73 @@ impl GPU {
         }
     }
 
+    fn step(&mut self, cycles: u8) -> Interrupts {
+        let mut request = Interrupts::None;
+        if !self.lcd_display_enabled {
+            return request;
+        }
+        self.cycles += cycles as u16;
+        let mode = self.draw_mode;
+        match mode {
+            Mode::Horizontal => {
+                if self.cycles >= 200 {
+                    self.cycles = self.cycles % 200;
+                    self.line += 1;
+
+                    if self.line >= 144 {
+                        self.draw_mode = Mode::Vertical;
+                        request.add(Interrupts::VBlank);
+                        if self.vblank_interrupt_enabled {
+                            request.add(Interrupts::LCDStat)
+                        }
+                    } else {
+                        self.draw_mode = Mode::OAM;
+                        if self.oam_interrupt_enabled {
+                            request.add(Interrupts::LCDStat)
+                        }
+                    }
+                    self.set_equal_lines_check(&mut request);
+                }
+            }
+            Mode::Vertical => {
+                if self.cycles >= 456 {
+                    self.cycles = self.cycles % 456;
+                    self.line += 1;
+                    if self.line == 154 {
+                        self.draw_mode = Mode::OAM;
+                        self.line = 0;
+                        if self.oam_interrupt_enabled {
+                            request.add(Interrupts::LCDStat)
+                        }
+                    }
+                    self.set_equal_lines_check(&mut request);
+                }
+            }
+            Mode::OAM => {
+                if self.cycles >= 80 {
+                    self.cycles = self.cycles % 80;
+                    self.draw_mode = Mode::VRAM;
+                }
+            }
+            Mode::VRAM => {
+                if self.cycles >= 172 {
+                    self.cycles = self.cycles % 172;
+                    if self.hblank_interrupt_enabled {
+                        request.add(Interrupts::LCDStat)
+                    }
+                    self.draw_mode = Mode::Horizontal;
+                }
+            }
+        }
+        request
+    }
+
+    fn set_equal_lines_check(&mut self, request: &mut Interrupts) {
+        let lines = self.line == self.line_check;
+        if lines && self.line_equals_line_check_interrupt_enabled {
+            request.add(Interrupts::LCDStat);
+        }
+        self.line_equals_line_check_interrupt_enabled = lines;
+    }
     
 }
